d42b41b2d0d503a1690e7dd582938c44722693b7
pklapuch <mr.pawel.klapuch@gmail.com>
2021-08-28 10:49:18 -0500

    [DEV] source code

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Pawel Klapuch on 8/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-Frameworks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks
+//
+//  Created by Pawel Klapuch on 8/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
